30b010e0faa9744bb47df71c6f935ef31fc7faf6
maximzobkov <56Vf5mg1>
2020-11-29 22:22:31 +0300

    start

---
+++ __init__.py
@@ -0,0 +1,163 @@
+import datetime
+import os
+
+from flask import Flask, render_template, redirect, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, DateField, BooleanField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+
+from data import db_session, users
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'GusStory.ru'
+db_session.global_init("db/blogs.sqlite")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    sessions = db_session.create_session()
+    return sessions.query(users.User).get(user_id)
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Логин", validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    email = StringField("Электронная почта", validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    date_of_birth = DateField('Дата рождения')
+    gender = SelectField('Пол', validators=[DataRequired()],
+                         choices=[('1', 'Мужской'), ('2', "Женский")])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LengthError(Exception):
+    error = 'Пароль должен от 8 до 15 символов!'
+
+
+class LetterError(Exception):
+    error = 'В пароле должна быть хотя бы одна буква!'
+
+
+class DigitError(Exception):
+    error = 'В пароле должна быть хотя бы одна цифра!'
+
+
+'''@app.route('/profile')
+def profile():
+    if current_user.is_authenticated:
+        return render_template("profile.html")
+    return redirect('/')'''
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        result = check_password(form.password.data)
+        date_check = check_date(form.date_of_birth.data)
+        if result != 'OK':
+            return render_template('register.html',
+                                   title='Регистрация',
+                                   form=form, email_error="OK", again_password_error="OK", date_error="OK",
+                                   password_error=result)
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form, email_error="OK", password_error="OK", date_error="OK",
+                                   again_password_error="Пароли не совпадают")
+        sessions = db_session.create_session()
+        if sessions.query(users.User).filter(users.User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   password_error="OK", again_password_error="OK", date_error="OK",
+                                   email_error="Такой пользователь уже есть")
+        if date_check != 'OK':
+            return render_template('register.html', title='Регистрация',
+                                   form=form, email_error="OK", password_error="OK",
+                                   again_password_error="OK", date_error=date_check)
+        user = users.User()
+        user.email = form.email.data.lower()
+        user.name = form.name.data
+        user.surname = form.surname.data
+        user.date_of_birth = form.date_of_birth.data
+        user.set_password(form.password.data)
+        sessions.add(user)
+        if form.gender.data == '1':
+            user.gender = 'Мужской'
+        else:
+            user.gender = 'Женский'
+        sessions.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form, email_error="OK",
+                           password_error="OK", again_password_error="OK", date_error='OK')
+
+
+def check_password(password):
+    flags = [0, 0]
+    for element in password:
+        if element.isdigit():
+            flags[0] = 1
+        elif element.isalpha():
+            flags[1] = 1
+    try:
+        if flags[1] == 0:
+            raise LetterError
+        if flags[0] == 0:
+            raise DigitError
+        if len(password) < 8 or len(password) > 15:
+            raise LengthError
+        return 'OK'
+    except (LengthError, LetterError, DigitError) as ex:
+        return ex.error
+
+
+def check_date(date):
+    if int(str(date).split('-')[0]) > 2015:
+        return 'Введенная дата неккоректна'
+    return "OK"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        sessions = db_session.create_session()
+        user = sessions.query(users.User).filter(users.User.email ==
+                                                 form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html', message='Неправильный логин или пароль', form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+def main():
+    global count_items
+    sessions = db_session.create_session()
+    # count_items += len(list(sessions.query(items.Items)))
+    sessions.close()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
